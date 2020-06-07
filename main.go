2148c5e86183d4012ad74210fcde69bdfd18adb4
Rafaela Rodrigues <ltcmdrdata@protonmail.com>
2020-06-07 17:32:41 -0300

    Aula 4 - Levantando um servidor

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, Go!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Rota OK"))
+	})
+
+	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+
+		data := map[string]string{
+			"message": "Trabalhando com objeto json",
+		}
+
+		result, _ := json.Marshal(data)
+
+		w.Write([]byte(result))
+	})
+
+	http.ListenAndServe(":3001", nil)
 }
